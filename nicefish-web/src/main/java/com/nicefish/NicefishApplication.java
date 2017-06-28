a76cee33468a04a00e437d6623c1a90630690c71
sunye <317484255@qq.com>
2017-06-28 15:18:33 +0800

    adjust NicifishApplication's path to com.nicefish in order to scan all sub packages

---
+++ nicefish-web/src/main/java/com/nicefish/NicefishApplication.java
@@ -0,0 +1,27 @@
+package com.nicefish;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*@SpringBootApplication(scanBasePackages={
+        "com.nicefish.config","com.nicefish.common","com.nicefish.service",
+        "com.nicefish.message","com.nicefish.swagger","com.nicefish.web"})*/
+@SpringBootApplication
+@RestController
+@MapperScan(basePackages = {"com.nicefish.mapper"})
+public class NicefishApplication {
+
+    @RequestMapping("/")
+    public String greeting() {
+
+        return "Hello World,kimmking!";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NicefishApplication.class, args);
+    }
+}
