62eeb770b62c8b5bf52d0a96fc1f70d3317a2179
sunye <317484255@qq.com>
2017-06-28 16:37:22 +0800

    configure swagger

--- nicefish-web/src/main/java/com/nicefish/web/controller/UserController.java
+++ nicefish-web/src/main/java/com/nicefish/web/controller/UserController.java
@@ -2,6 +2,9 @@ package com.nicefish.web.controller;
 
 import com.nicefish.model.User;
 import com.nicefish.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,19 +19,23 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/login")
+    /*@RequestMapping("/login")
     public User login(@RequestParam String userName,@RequestParam String password) {
         return userService.findByUserNameAndPassword(userName,password);
-    }
-
-    @RequestMapping("/id/{id}")
+    }*/
+    @ApiOperation(value = "login", nickname = "login",response = User.class)
+    @ApiResponses({
+            @ApiResponse(code = 404, response = String.class, message = "user not found"),
+            @ApiResponse(code = 500, response = String.class, message = "Internal server error")
+    })
+    @RequestMapping(path="/id/{id}",method = RequestMethod.GET)
     public User login(@PathVariable("id") String userId) {
         return userService.findByUserId(userId);
     }
 
-    @RequestMapping("/register")
+    /*@RequestMapping("/register")
     public User register(@RequestBody User user) {
         user.setUserId(java.util.UUID.randomUUID().toString());
         return userService.save(user);
-    }
+    }*/
 }
